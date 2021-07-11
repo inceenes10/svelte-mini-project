ab6d469f552a345f85b24b351884bf8dd0d63071
Enes İnce <inceenes10@gmail.com>
2021-07-11 23:59:27 +0300

    first commit

---
+++ src/AddPersonForm.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    let dispatch = createEventDispatcher();
+
+    let name, beltColour, age, skills = [];
+    const handleSubmit = () => {
+        const person = {
+            name, beltColour, age, skills, id: Math.random()
+        };
+        dispatch("addPerson", person);
+    }
+</script>
+
+
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="name" bind:value={name}/>
+    <input type="number" placeholder="age" bind:value={age}/>
+    <span>Skills</span>
+    <input type="checkbox" bind:group={skills} value="coding"/>Coding<br />
+    <input type="checkbox" bind:group={skills} value="running"/>Running<br />
+    <input type="checkbox" bind:group={skills} value="sneaking"/>Sneaking<br />
+    <span>Belt Colour:</span>
+    <select bind:value={beltColour}>
+        <option value="black">Black</option>
+        <option value="orange">Orange</option>
+        <option value="brown">Brown</option>
+        <option value="white">White</option>
+    </select>
+    <button>Add Person</button>
+</form>
+
+<style>
+</style>
---
+++ src/Modal.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let showModal = true;
+    export let isPromo = false;
+</script>
+
+{#if showModal}
+<div class="backdrop" class:promo={isPromo} on:click|self>
+    <div class="modal">
+        <slot></slot>
+    </div>
+</div>
+{/if}
+
+<style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.7);
+        display: grid;
+        place-content: center;
+        cursor: pointer;
+    }
+
+    .modal {
+        cursor: initial;
+        width: 400px;
+        padding: 10px;
+        border-radius: 10px;
+        text-align: center;
+        background: white;
+    }
+    .promo .modal {
+        background: crimson;
+        color: white;
+    }
+</style>
